2433348ff233a78491f75beda9286e4103982a66
Nathanael Schmidt-Ott <n.schmidt.ott@gmail.com>
2023-08-17 17:37:40 +0200

    add functions to plot the satellite images

--- analysis/analysis_utils/plotting.py
+++ analysis/analysis_utils/plotting.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy.ma as ma
-
+import os
 #******************************************************************************
 #........ Functions to plot Landsat images ........
 #******************************************************************************
@@ -134,7 +134,7 @@ def bytescale(data, high=255, low=0, cmin=None, cmax=None):
 def plot_ls(cluster_id, fname = None, title = None):
     data_type = 'LS'
     sat_img_dir = "../../Data/satellite_imgs"
-    img_dir = f"{sat_img_dir}/satellite_imgs/{data_type}/LS_median_cluster"
+    img_dir = f"{sat_img_dir}/{data_type}/LS_median_cluster"
     img_pth = f"{img_dir}/{data_type}_{cluster_id}.npy"
 
     ls_img = np.load(img_pth)#.transpose(2,0,1)
@@ -149,25 +149,8 @@ def plot_ls(cluster_id, fname = None, title = None):
 
 
 #******************************************************************************
-#........ Functions to plot other RS images ........
+#........ Functions to plot static RS images ........
 #******************************************************************************
-
-def plot_nl(unique_id, fname = None, title = None):
-    data_type = 'RS_v2'
-    sat_img_dir = "../../Data/satellite_imgs"
-    img_dir = f"{sat_img_dir}/RS_v2/{data_type}_dynamic_processed"
-    img_pth = f"{img_dir}/{data_type}_{unique_id}.npy"
-
-    dynamic_img = np.load(img_pth)
-
-    # plot the nl img
-    plt.imshow(dynamic_img[:,:,0], cmap = 'gray')
-    plt.title(title)
-    plt.axis('off')
-    if fname is not None:
-        pth = f"../figures/sat_imgs/{fname}"
-        plt.savefig(pth, dpi = 300, bbox_inches = 'tight')
-    plt.show()
 
 def plot_wsf(cluster_id, fname = None, title = None):
     data_type = 'RS_v2'
@@ -176,7 +159,7 @@ def plot_wsf(cluster_id, fname = None, title = None):
     img_pth = f"{img_dir}/{data_type}_{cluster_id}.npy"
 
     static_img = np.load(img_pth)
-    plt.imshow(static_img[:,:,1], cmap = 'gray')
+    plt.imshow(static_img[:,:,0], cmap = 'gray')
     plt.axis('off')
     plt.title(title)
     if fname is not None:
@@ -187,7 +170,7 @@ def plot_wsf(cluster_id, fname = None, title = None):
 def plot_lc_esa(cluster_id, fname = None, title = None):
     data_type = 'RS_v2'
     sat_img_dir = "../../Data/satellite_imgs"
-    img_dir = f"{sat_img_dir}/RS_v2/{data_type}_processed"
+    img_dir = f"{sat_img_dir}/RS_v2/{data_type}_static_processed"
 
     img_pth = f"{img_dir}/{data_type}_{cluster_id}.npy"
     static_img = np.load(img_pth)
@@ -199,13 +182,13 @@ def plot_lc_esa(cluster_id, fname = None, title = None):
     labels = ['Tree cover', 'Shrubland', 'Grassland', 'Cropland', 'Built-up',
               'Barren', 'Wetland', 'Water']
 
-    values = [10,20,30,40,50,60,70,80]
+    values = [.1,.2,.3,.4,.5,.6,.7,.8]
 
     # Create a custom colormap with discrete colors
     cmap = plt.cm.colors.ListedColormap(colors)
 
     # Normalize the image data for mapping the values to the colormap
-    norm = plt.cm.colors.Normalize(vmin=10, vmax=80)
+    norm = plt.cm.colors.Normalize(vmin=.1, vmax=.8)
 
     # Plot the image with the custom color map
     image = plt.imshow(static_img[:,:,1], cmap=cmap, norm=norm)
@@ -213,7 +196,7 @@ def plot_lc_esa(cluster_id, fname = None, title = None):
     plt.title(title)
 
     # Create colorbar with labels
-    tick_values = np.linspace(10,80,7)
+    tick_values = np.linspace(.15,.75,8)
     cbar = plt.colorbar(image, ticks=tick_values)#, fraction=0.046, pad=0.04)
     cbar.ax.set_yticklabels(labels)
 
@@ -224,32 +207,6 @@ def plot_lc_esa(cluster_id, fname = None, title = None):
 
     plt.show()
 
-def plot_ndvi(unique_id, fname = None, title = None):
-    data_type = 'RS_V2'
-    sat_img_dir = "../../Data/satellite_imgs"
-    img_dir = f"{sat_img_dir}/RS_v2/RS_v2_dynamic_processed"
-    img_pth = f"{img_dir}/{data_type}_{unique_id}.npy"
-
-    dynamic_img = np.load(img_pth)
-    color_palette = [
-        '#ffffff', '#ce7e45', '#df923d', '#f1b555', '#fcd163', '#99b718', '#74a901',
-        #'#66a000', '#529400', '#3e8601', '#207401', '#056201', '#004c00', '#023b01',
-        '#012e01', '#011d01', '#011301']
-
-    # Create a custom colormap using the defined color palette
-    cmap = plt.cm.colors.LinearSegmentedColormap.from_list('custom', color_palette, N=256)
-
-    # Plot the image
-    plt.imshow(dynamic_img[:,:,1], cmap=cmap, vmin=-1, vmax=1)
-    plt.axis('off')
-    plt.title(title)
-
-    # Display the plot
-    if fname is not None:
-        pth = f"../figures/sat_imgs/{fname}"
-        plt.savefig(pth, dpi = 300, bbox_inches = 'tight')
-
-    plt.show()
 
 def plot_lc_modis(unique_id, fname = None, title = None):
     data_type = 'NL_WSF_LC'
@@ -288,11 +245,268 @@ def plot_lc_modis(unique_id, fname = None, title = None):
     cbar.ax.set_yticklabels(labels)
 
     if fname is not None:
-        pth = f"descriptives/sat_imgs/{fname}"
+        pth = f"../figures/sat_imgs/{fname}"
         plt.savefig(pth, dpi = 300, bbox_inches = 'tight')
     plt.show()
 
-def plot_target_by_cntry(lsms_df, target_var, fname):
+
+#******************************************************************************
+#........ Functions to plot dynamic RS images ........
+#******************************************************************************
+
+
+def plot_nl(unique_id, fname = None, title = None):
+    data_type = 'RS_v2'
+    sat_img_dir = "../../Data/satellite_imgs"
+    img_dir = f"{sat_img_dir}/RS_v2/{data_type}_dynamic_processed"
+    img_pth = f"{img_dir}/{data_type}_{unique_id}.npy"
+
+    dynamic_img = np.load(img_pth)
+
+    # plot the nl img
+    plt.imshow(dynamic_img[:,:,0], cmap = 'gray')
+    plt.title(title)
+    plt.axis('off')
+    if fname is not None:
+        pth = f"../figures/sat_imgs/{fname}"
+        plt.savefig(pth, dpi = 300, bbox_inches = 'tight')
+    plt.show()
+
+def plot_ndvi(unique_id, fname = None, title = None):
+    data_type = 'RS_V2'
+    sat_img_dir = "../../Data/satellite_imgs"
+    img_dir = f"{sat_img_dir}/RS_v2/RS_v2_dynamic_processed"
+    img_pth = f"{img_dir}/{data_type}_{unique_id}.npy"
+
+    dynamic_img = np.load(img_pth)
+    color_palette = [
+        '#ffffff', '#ce7e45', '#df923d', '#f1b555', '#fcd163', '#99b718', '#74a901',
+        #'#66a000', '#529400', '#3e8601', '#207401', '#056201', '#004c00', '#023b01',
+        '#012e01', '#011d01', '#011301']
+
+    # Create a custom colormap using the defined color palette
+    cmap = plt.cm.colors.LinearSegmentedColormap.from_list('custom', color_palette, N=256)
+
+    # Plot the image
+    plt.imshow(dynamic_img[:,:,1], cmap=cmap, vmin=-1, vmax=1)
+    plt.axis('off')
+    plt.title(title)
+
+    # Display the plot
+    if fname is not None:
+        pth = f"../figures/sat_imgs/{fname}"
+        plt.savefig(pth, dpi = 300, bbox_inches = 'tight')
+
+    plt.show()
+
+def plot_nl_cluster(cluster_id, fname = None, title = None):
+    data_type = 'RS_v2'
+    sat_img_dir = "../../Data/satellite_imgs"
+    img_dir = f"{sat_img_dir}/RS_v2/{data_type}_dynamic_processed"
+
+    # list all files in image directory
+    files = np.array(os.listdir(img_dir))
+    mask = [cluster_id in i for i in files]
+    files = np.sort(files[mask])
+
+    # plot all images in one plot
+    # Plot all images in one plot
+    num_images = len(files)
+    rows = 1
+    cols = num_images
+
+    fig, axes = plt.subplots(rows, cols, figsize=(10, 5))
+
+    for i, file in enumerate(files):
+        year = file[-8:-4]
+        img_pth = os.path.join(img_dir, file)
+        img = np.load(img_pth)[:,:,0]
+        ax = axes[i]
+        ax.imshow(img)
+        ax.axis('off')
+
+        ax.set_title(f"{year}")
+    plt.tight_layout()
+
+    if fname is not None:
+        pth = f"../figures/sat_imgs/{fname}"
+        plt.savefig(pth, dpi=300, bbox_inches='tight')
+
+    plt.show()
+
+
+def plot_ndvi_cluster(cluster_id, fname=None, title=None):
+    data_type = 'RS_v2'
+    sat_img_dir = "../../Data/satellite_imgs"
+    img_dir = f"{sat_img_dir}/RS_v2/{data_type}_dynamic_processed"
+
+    # list all files in image directory
+    files = np.array(os.listdir(img_dir))
+    mask = [cluster_id in i for i in files]
+    files = np.sort(files[mask])
+
+    # plot all images in one plot
+    # Plot all images in one plot
+    num_images = len(files)
+    rows = 1
+    cols = num_images
+
+    fig, axes = plt.subplots(rows, cols, figsize=(10, 5))
+
+    color_palette = [
+        '#ffffff', '#ce7e45', '#df923d', '#f1b555', '#fcd163', '#99b718', '#74a901',
+        # '#66a000', '#529400', '#3e8601', '#207401', '#056201', '#004c00', '#023b01',
+        '#012e01', '#011d01', '#011301']
+
+    # Create a custom colormap using the defined color palette
+    cmap = plt.cm.colors.LinearSegmentedColormap.from_list('custom', color_palette, N=256)
+
+    # Plot the image
+    for i, file in enumerate(files):
+        year = file[-8:-4]
+        img_pth = os.path.join(img_dir, file)
+        img = np.load(img_pth)[:, :, 1]
+        ax = axes[i]
+        ax.imshow(img, cmap=cmap, vmin=-1, vmax=1)
+        ax.axis('off')
+
+        ax.set_title(f"{year}")
+    plt.tight_layout()
+
+    if fname is not None:
+        pth = f"../figures/sat_imgs/{fname}"
+        plt.savefig(pth, dpi=300, bbox_inches='tight')
+
+    plt.show()
+
+def plot_ndwi_gao_cluster(cluster_id, fname=None, title=None):
+    data_type = 'RS_v2'
+    sat_img_dir = "../../Data/satellite_imgs"
+    img_dir = f"{sat_img_dir}/RS_v2/{data_type}_dynamic_processed"
+
+    # list all files in image directory
+    files = np.array(os.listdir(img_dir))
+    mask = [cluster_id in i for i in files]
+    files = np.sort(files[mask])
+
+    # plot all images in one plot
+    # Plot all images in one plot
+    num_images = len(files)
+    rows = 1
+    cols = num_images
+
+    fig, axes = plt.subplots(rows, cols, figsize=(10, 5))
+
+    color_palette = ['#F4F4F6', '#000E75']
+
+    # Create a custom colormap using the defined color palette
+    cmap = plt.cm.colors.LinearSegmentedColormap.from_list('custom', color_palette, N=256)
+
+    # Plot the image
+    for i, file in enumerate(files):
+        year = file[-8:-4]
+        img_pth = os.path.join(img_dir, file)
+        img = np.load(img_pth)[:, :, 2]
+        ax = axes[i]
+        ax.imshow(img, cmap=cmap, vmin=-1, vmax=1)
+        ax.axis('off')
+
+        ax.set_title(f"{year}")
+    plt.tight_layout()
+
+    if fname is not None:
+        pth = f"../figures/sat_imgs/{fname}"
+        plt.savefig(pth, dpi=300, bbox_inches='tight')
+
+    plt.show()
+
+
+def plot_ndwi_mcf_cluster(cluster_id, fname=None, title=None):
+    data_type = 'RS_v2'
+    sat_img_dir = "../../Data/satellite_imgs"
+    img_dir = f"{sat_img_dir}/RS_v2/{data_type}_dynamic_processed"
+
+    # list all files in image directory
+    files = np.array(os.listdir(img_dir))
+    mask = [cluster_id in i for i in files]
+    files = np.sort(files[mask])
+
+    # plot all images in one plot
+    # Plot all images in one plot
+    num_images = len(files)
+    rows = 1
+    cols = num_images
+
+    fig, axes = plt.subplots(rows, cols, figsize=(10, 5))
+
+    color_palette = ['#F4F4F6', '#000E75']
+
+    # Create a custom colormap using the defined color palette
+    cmap = plt.cm.colors.LinearSegmentedColormap.from_list('custom', color_palette, N=256)
+
+    # Plot the image
+    for i, file in enumerate(files):
+        year = file[-8:-4]
+        img_pth = os.path.join(img_dir, file)
+        img = np.load(img_pth)[:, :, 3]
+        ax = axes[i]
+        ax.imshow(img, cmap=cmap, vmin=-1, vmax=1)
+        ax.axis('off')
+
+        ax.set_title(f"{year}")
+    plt.tight_layout()
+
+    if fname is not None:
+        pth = f"../figures/sat_imgs/{fname}"
+        plt.savefig(pth, dpi=300, bbox_inches='tight')
+
+    plt.show()
+
+
+#******************************************************************************
+#........ Functions to plot demeaned / delta images ........
+#******************************************************************************
+    def plot_demeaned_imgs(cluster_id, fname=None, title=None, channel=0):
+        data_type = 'RS_v2'
+        sat_img_dir = "../../Data/satellite_imgs"
+        img_dir = f"{sat_img_dir}/RS_v2/{data_type}_demeaned"
+
+        # list all files in image directory
+        files = np.array(os.listdir(img_dir))
+        mask = [cluster_id in i for i in files]
+        files = np.sort(files[mask])
+
+        # plot all images in one plot
+        num_images = len(files)
+        rows = 1
+        cols = num_images
+
+        fig, axes = plt.subplots(rows, cols, figsize=(10, 5))
+
+        for i, file in enumerate(files):
+            year = file[-8:-4]
+            img_pth = os.path.join(img_dir, file)
+            img = np.load(img_pth)[:, :, channel]
+            ax = axes[i]
+            ax.imshow(img)
+            ax.axis('off')
+
+            ax.set_title(f"{year}")
+        plt.tight_layout()
+        # plt.legend()
+
+        if fname is not None:
+            pth = f"../figures/sat_imgs/{fname}"
+            plt.savefig(pth, dpi=300, bbox_inches='tight')
+
+        plt.show()
+
+
+#******************************************************************************
+#........ Functions to plot target variables ........
+#******************************************************************************
+
+def plot_target_by_cntry(lsms_df, target_var, fname = None):
     plt.figure(figsize=(5, 5))
     for cntry in np.unique(lsms_df.country):
         # subset the data
@@ -304,7 +518,29 @@ def plot_target_by_cntry(lsms_df, target_var, fname):
     plt.xlabel("Year")
     plt.ylabel(target_var)
     plt.legend()
-    plt.savefig(f"../figures/target_vars/{fname}", dpi = 300, bbox_inches = 'tight')
+    if fname is not None:
+        plt.savefig(f"../figures/target_vars/{fname}", dpi = 300, bbox_inches = 'tight')
     plt.show()
+
+
+def plot_target_cluster(cluster_id, lsms_df, target_var, fname = None):
+    plt.figure(figsize = (6,6))
+    # subset the data
+    sub_df = lsms_df[lsms_df['cluster_id'] == cluster_id].reset_index(drop = True).sort_values('start_year')
+
+    plt.plot(sub_df['start_year'], sub_df[target_var])
+    plt.scatter(sub_df['start_year'], sub_df[target_var])
+    x_ticks = range(min(sub_df['start_year']), max(sub_df['start_year'])+1)
+    plt.xticks(x_ticks)
+    plt.xlabel("Year")
+    plt.ylabel(target_var)
+    if fname is not None:
+        plt.savefig(f"../figures/target_vars/{fname}", bbox_inches = 'tight', dpi = 300)
+    plt.show()
+
+
+
+
+
 
 
