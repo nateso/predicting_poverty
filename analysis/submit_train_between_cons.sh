9d02be385cd5ab47b69d361758d2a2f7bf9a46f3
Nathanael Schmidt-Ott <n.schmidt.ott@gmail.com>
2023-09-29 14:51:34 +0200

    change runtime

--- analysis/submit_train_between_cons.sh
+++ analysis/submit_train_between_cons.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=cons_between               # Job name
-#SBATCH -t 00:30:00                   # estimated time
+#SBATCH -t 01:00:00                   # estimated time
 #SBATCH -p gpu                     # the partition you are training on (i.e., which nodes), for nodes see sinfo -p grete:shared --format=%N,%G
 #SBATCH -G v100:1              # Add the type of GPU used
 #SBATCH -C scratch                    # ensure that I work on a node that has access to scratch
